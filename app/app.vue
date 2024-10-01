3c5d780e54ea1130f3b1ffc90ac41ef93b0c8c4c
Ahmad Yogi Al Fandi <alfandi0857@gmail.com>
2024-10-01 12:48:57 +0700

    create index and about page, and create fetch data

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+// nuxt page ini unutuk folder page sedangkan nuxt layout untul folder layout
+</script>
 <template>
-  <div class="font-Montserrat">asdas</div>
+  <div class="font-Montserrat container">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
